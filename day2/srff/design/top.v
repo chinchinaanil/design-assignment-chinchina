4193c294c6edd9edeae3fc5a8e719a52f83af663
chinchinaanil <chinchinaanil2005@gmail.com>
2026-06-11 19:16:39 +0530

    Add SR flip-flop module implementation

--- day2/srff/design/top.v
+++ day2/srff/design/top.v
@@ -1,1 +1,18 @@
+module sr_ff(
+    input clk,
+    input s,
+    input r,
+    output reg q
+);
 
+always @(posedge clk)
+begin
+    case ({s,r})
+        2'b00: q <= q;      // No Change
+        2'b01: q <= 1'b0;   // Reset
+        2'b10: q <= 1'b1;   // Set
+        2'b11: q <= 1'bx;   // Invalid State
+    endcase
+end
+
+endmodule
