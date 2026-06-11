a1f62c91de6ceef50effac236880e1c813d59438
chinchinaanil <chinchinaanil2005@gmail.com>
2026-06-11 17:33:38 +0530

    Add testbench for 2-to-4 decoder

--- day2/decoder/tb/tb_top.v
+++ day2/decoder/tb/tb_top.v
@@ -1,1 +1,27 @@
+module tb_decoder_2to4;
 
+reg [1:0] A;
+wire [3:0] Y;
+
+// Instantiate the decoder
+decoder_2to4 DUT (
+    .A(A),
+    .Y(Y)
+);
+
+initial
+begin
+    A = 2'b00; #10;
+    A = 2'b01; #10;
+    A = 2'b10; #10;
+    A = 2'b11; #10;
+
+    $finish;
+end
+
+initial
+begin
+    $monitor("Time=%0t A=%b Y=%b", $time, A, Y);
+end
+
+endmodule
