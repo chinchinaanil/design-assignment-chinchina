63fd31324fac7579ae9f68ed0e096ea7ab0fa5d3
chinchinaanil <chinchinaanil2005@gmail.com>
2026-06-15 21:46:30 +0530

    Create fifo_transaction class for FIFO operations
    Added fifo_transaction class with inputs, outputs, and constraints.

--- Day6/FIFO transaction class/Testbench/Top.v
+++ Day6/FIFO transaction class/Testbench/Top.v
@@ -1,1 +1,24 @@
+class fifo_transaction;
 
+  // FIFO Inputs
+  rand bit        wr_en;
+  rand bit        rd_en;
+  rand bit [7:0]  din;
+
+  // FIFO Outputs
+  bit [7:0] dout;
+  bit       full;
+  bit       empty;
+
+  // Constraint: avoid read and write together
+  constraint c1 {
+    !(wr_en && rd_en);
+  }
+
+  // Display Method
+  function void display();
+    $display("wr_en=%0b rd_en=%0b din=%0h dout=%0h full=%0b empty=%0b",
+              wr_en, rd_en, din, dout, full, empty);
+  endfunction
+
+endclass
