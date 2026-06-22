984119240cf16e2ed9002826abeea7d853daeff3
chinchinaanil <chinchinaanil2005@gmail.com>
2026-06-22 22:15:30 +0530

    Define APB interface with necessary signals

--- day7/APB/simulation/interface.sv
+++ day7/APB/simulation/interface.sv
@@ -1,1 +1,10 @@
-
+interface apb_if(input logic clk);
+  logic        rst_n;
+  logic [31:0] paddr;
+  logic        psel;
+  logic        penable;
+  logic        pwrite;
+  logic [31:0] pwdata;
+  logic [31:0] prdata;
+  logic        pready;
+endinterface
