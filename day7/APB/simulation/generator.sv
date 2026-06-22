faf3eb77683d59fe909e2efa4b045ec37d11124e
chinchinaanil <chinchinaanil2005@gmail.com>
2026-06-22 22:13:45 +0530

    Add generator class for APB transaction simulation

--- day7/APB/simulation/generator.sv
+++ day7/APB/simulation/generator.sv
@@ -1,1 +1,24 @@
+class generator;
+  apb_mbx gen2drv; 
+  int num_pairs = 10; 
 
+  function new(apb_mbx gen2drv);
+    this.gen2drv = gen2drv;
+  endfunction
+
+  task main();
+    repeat(num_pairs) begin
+      apb_transaction tr_write = new();
+      apb_transaction tr_read  = new();
+      
+      if (!tr_write.randomize() with { write == 1'b1; }) 
+        $fatal(1, "Randomization failed");
+      gen2drv.put(tr_write);
+      
+      if (!tr_read.randomize() with { write == 1'b0; }) 
+        $fatal(1, "Randomization failed");
+      tr_read.addr = tr_write.addr; 
+      gen2drv.put(tr_read);
+    end
+  endtask
+endclass
