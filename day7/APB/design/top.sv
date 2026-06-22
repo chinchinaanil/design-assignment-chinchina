112c9f89a0664357201d030f5a4255c4d09f718a
chinchinaanil <chinchinaanil2005@gmail.com>
2026-06-22 22:04:16 +0530

    Add top module for APB design

--- day7/APB/design/top.sv
+++ day7/APB/design/top.sv
@@ -1,1 +1,25 @@
+module top (
+  input  logic        clk,
+  input  logic        rst_n,
+  input  logic [31:0] paddr,
+  input  logic        psel,
+  input  logic        penable,
+  input  logic        pwrite,
+  input  logic [31:0] pwdata,
+  output logic [31:0] prdata,
+  output logic        pready
+);
 
+  apb_slave u_apb_slave (
+    .clk    (clk),
+    .rst_n  (rst_n),
+    .paddr  (paddr),
+    .psel   (psel),
+    .penable(penable),
+    .pwrite (pwrite),
+    .pwdata (pwdata),
+    .prdata (prdata),
+    .pready (pready)
+  );
+
+endmodule
