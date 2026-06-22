d45f2293feae0430ec7798fc4289bfd7e8b9388f
chinchinaanil <chinchinaanil2005@gmail.com>
2026-06-22 22:21:17 +0530

    Add apb_transaction class with constraints

--- day7/APB/simulation/transaction.sv
+++ day7/APB/simulation/transaction.sv
@@ -1,1 +1,14 @@
+class apb_transaction;
+  rand bit [31:0] addr;
+  rand bit [31:0] wdata;
+  rand bit        write;
+  bit [31:0]      rdata;
 
+  constraint c_addr { 
+    addr[1:0] == 2'b00;       
+    addr >= 32'h0000_0000;    
+    addr <= 32'h0000_03FC;    
+  } 
+endclass
+
+typedef mailbox #(apb_transaction) apb_mbx;
